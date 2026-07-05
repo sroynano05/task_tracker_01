d13b45d6de24c2cdfcf442f065f37bad2dde7aed
sroynano05 <sroynano05@gmail.com>
2026-07-05 09:12:33 +0000

    add index.js featurs

---
+++ command/command.js
@@ -0,0 +1,13 @@
+import cache from "../index.js"
+export function add(){
+
+}
+export function update(){
+
+}
+export function done(){
+
+}
+export function del(){
+
+}
---
+++ index.js
@@ -0,0 +1,20 @@
+```
+Before procedding,Here is a basic guidlince to operate this application : \n
+
+```
+import { argv } from 'node:process';
+import {add,done,del,update} from "../task_tracker(jst chill')/command/command.js"
+let cache=[];
+argv.forEach((val, index) => {
+    if(index>=2) cache.push(val)
+});
+const command={"add":add(),"done":done(),"delete":del(),"update":update()}
+const hashed_key=Object.keys(command).find(key=>key.includes(cache[0]))
+
+if(hashed_key){
+    const cmd=command[hashed_key]
+    cmd();
+}else{
+    console.log("fatal:incorrect command")
+}
+export default cache
