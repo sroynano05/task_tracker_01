2717495898a70b2f94ef75b453ccc7da47faad79
Suman Roy <165665087+sroynano05@users.noreply.github.com>
2026-09-12 02:04:25 +0530

    Refactor command handling in index.js

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 #!/usr/bin/env node
 import { argv } from 'node:process';
-import {add,done,del,update,read} from "../task_tracker(jst chill')/command/command.js"
+import {add,mark,del,update,read} from "../task_tracker(jst chill')/command/command.js"
 let cache=[];
 argv.forEach((val, index) => {
     if(index>=2) cache.push(val)
 });
-const command={add:add,done:done,delete:del,update:update,read:read}
+const command={add:add,mark:mark,delete:del,update:update,read:read}
 if (cache[0]==command.read) {
     read(cache);
 }
